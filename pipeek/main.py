2132343d9a91fe66b9c88f62cad6d8d47595eb30
Danilo Patrial <danmcamposp@gmail.com>
2025-07-17 17:19:13 -0300

    Add peek at

--- pipeek/main.py
+++ pipeek/main.py
@@ -1,19 +1,19 @@
 # pipeek/main.py
 
 # MIT License
-# 
+#
 # Copyright (c) 2025 Danilo Patrial
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,7 +34,6 @@ import bz2
 import lzma
 import sys
 import io
-import pathlib
 import collections
 import warnings
 import time
@@ -53,13 +52,13 @@ except (ImportError, NameError):
 
     warnings.warn(
         "Colorama is not installed. Colored terminal output may not work properly.\n"
-        "To fix this, install Colorama by running: pip install colorama\n"
-        "Or install all dependencies using: pip install -r requirements.txt",
+        "To fix this, install Colorama by running: pip install colorama\n",
         category=UserWarning,
     )
 
 from importlib.metadata import version, PackageNotFoundError
 from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 
 
 # --- Configuration File ------------------------------------------------------------------------------------
@@ -70,7 +69,12 @@ config_path = os.path.join(config_dir, "config.json")
 
 os.makedirs(config_dir, exist_ok=True)
 
-base_config: dict = {"buffer_size": "8M", "max_matches": 0, "around_context": 10}
+base_config: dict = {
+    "buffer_size": "8M",
+    "max_matches": 0,
+    "around_context": 10,
+    "haystack_path": None,
+}
 
 
 # --- Logging -----------------------------------------------------------------------------------------------
@@ -139,14 +143,12 @@ def _iter_needle_indexes(haystack: bytes, needle: bytes, /) -> t.Iterator[int]:
 # --- Steam-Search Engine -----------------------------------------------------------------------------------
 
 
-def open_stream(
-    file: str | pathlib.Path, /, *, force_gzip: bool = False
-) -> io.BufferedReader:
+def open_stream(file: str | Path, /, *, force_gzip: bool = False) -> io.BufferedReader:
     """Return a binary buffered stram for txt or compressed files."""
 
-    path: pathlib.Path = pathlib.Path(file)
+    path: Path = Path(file)
 
-    if path == pathlib.Path("-"):  # stdin support
+    if path == Path("-"):  # stdin support
         return sys.stdin.buffer
 
     ext: str = path.suffix.lower()
@@ -161,6 +163,57 @@ def open_stream(
         return lzma.open(path, "rb")
 
     return path.open("rb")
+
+
+def walk(
+    haystack: tuple[Path],
+    handler: t.Callable[[Path, bool], None],
+    force_gzip: bool,
+) -> None:
+
+    streams: list[Path] = [Path(p) for p in haystack]
+    for path_obj in streams:
+        if not path_obj.is_dir():
+            handler(path_obj, force_gzip)
+            continue
+
+        for dirpath, _, filenames in os.walk(path_obj):
+            for filename in filenames:
+                handler(Path(dirpath) / filename, force_gzip)
+
+
+def peek_at(
+    index: int, haystack: str | Path | None, length: int, force_gzip: int
+) -> t.NoReturn:
+
+    config_info: dict = _access_config()
+    start: float = time.time()
+
+    haystack = (config_info["haystack_path"],) if not haystack else haystack
+    arround_context: int = config_info["around_context"]
+
+    def handler(filepath: str | Path, force_gzip: bool) -> None:
+        file_base_name: str = os.path.basename(filepath)
+
+        with open_stream(filepath, force_gzip=force_gzip) as file:
+            file.seek(index - arround_context)
+            chunk: bytes = file.read(length + 2 * arround_context)
+
+            offset = max(0, arround_context)
+            left_context: bytes = chunk[:offset]
+            needle: bytes = chunk[offset : offset + length]
+            right_context: bytes = chunk[offset + length:]
+
+            elapsed: float = time.time() - start
+            match: Match = Match(index, left_context, right_context)
+
+            print(_render_match(match, needle, elapsed))
+            logging.info(
+                f'filename="{file_base_name}"; {match}; {needle=}; {elapsed=}'
+            )
+
+    walk(haystack, handler, force_gzip)
+    sys.exit(0)
 
 
 def scan_stream(
@@ -182,9 +235,7 @@ def scan_stream(
 
             absolute_position: int = position + idx
             left_context: bytes = chunk[max(0, idx - around) : idx]
-            right_context: bytes = chunk[
-                idx + len_pattern : idx + len_pattern + around
-            ]
+            right_context: bytes = chunk[idx + len_pattern : idx + len_pattern + around]
 
             yield Match(absolute_position, left_context, right_context)
 
@@ -194,7 +245,7 @@ def scan_stream(
 
 def peek_needle(
     needle: str,
-    haystack: str | pathlib.Path,
+    haystack: str | Path | None,
     force_gzip: bool = False,
 ) -> t.NoReturn:
 
@@ -203,14 +254,14 @@ def peek_needle(
 
     needle: bytes = needle.encode()
 
+    haystack = (config_info["haystack_path"],) if not haystack else haystack
     buffer_size: int = _parse_bytes(config_info["buffer_size"])
     arround_context: int = config_info["around_context"]
     max_matches: int = config_info["max_matches"]
 
     hit: bool = False
 
-    def handler(filepath: str | pathlib.Path, force_gzip: bool) -> None:
-
+    def handler(filepath: str | Path, force_gzip: bool) -> None:
         file_base_name: str = os.path.basename(filepath)
 
         with open_stream(filepath, force_gzip=force_gzip) as file:
@@ -229,15 +280,7 @@ def peek_needle(
                 if max_matches != 0 and (i + 1) >= max_matches:
                     sys.exit(0 if hit else 1)
 
-    streams: list[pathlib.Path] = [pathlib.Path(p) for p in haystack]
-    for path_obj in streams:
-        if path_obj.is_dir():
-            for dirpath, _, filenames in os.walk(path_obj):
-                for filename in filenames:
-                    handler(pathlib.Path(dirpath) / filename, force_gzip)
-        else:
-            handler(path_obj, force_gzip)
-
+    walk(haystack, handler, force_gzip)
     sys.exit(0 if hit else 1)
 
 
@@ -295,7 +338,9 @@ def conf(editor: str, restore: bool) -> None:
 
 @cli.command()
 @click.argument("needle")
-@click.argument("haystack", nargs=-1, type=click.Path(exists=True), required=False)
+@click.argument(
+    "haystack", nargs=-1, type=click.Path(exists=True), required=False, default=None
+)
 @click.option(
     "-f",
     "--force-gzip",
@@ -304,7 +349,25 @@ def conf(editor: str, restore: bool) -> None:
     default=False,
     help="Force gunzip read",
 )
-
-def needle(needle: str, haystack: str, force_gzip: bool) -> None:
+def needle(needle: str, haystack: str | None, force_gzip: bool) -> None:
     """Find a needle in a haystack"""
     peek_needle(needle, haystack, force_gzip)
+
+
+@cli.command()
+@click.argument("index", type=int)
+@click.argument(
+    "haystack", nargs=-1, type=click.Path(exists=True), required=False, default=None
+)
+@click.option("--len", "len", type=int, default=10)
+@click.option(
+    "-f",
+    "--force-gzip",
+    "force_gzip",
+    is_flag=True,
+    default=False,
+    help="Force gunzip read",
+)
+def at(index: int, haystack: str | None, len: int, force_gzip: bool) -> None:
+    """Peek at a given index."""
+    peek_at(index, haystack, len, force_gzip)
